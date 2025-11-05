76b8fb00bcaa3033d9313d9a12d07a9326c0a354
Rujian <roman.2006.08.18@gmail.com>
2025-11-05 23:54:35 +0300

    create screens switch

---
+++ frontend/lib/app/pages/main_screen.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+import 'package:cosmetics_store/features/cart/presentation/pages/cart_screen.dart';
+import 'package:cosmetics_store/features/catalog/presentation/pages/catalog_screen.dart';
+import 'package:cosmetics_store/features/home/presentation/pages/home_screen.dart';
+import 'package:cosmetics_store/features/profile/presentation/pages/profile_screen.dart';
+
+class MainScreen extends StatefulWidget {
+  const MainScreen({super.key});
+
+  @override
+  State<MainScreen> createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen> {
+  int _currentIndex = 0;
+
+  final List<Widget> _screens = [
+    const HomeScreen(),
+    const CatalogScreen(),
+    const CartScreen(),
+    const ProfileScreen(),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: IndexedStack(index: _currentIndex, children: _screens),
+      bottomNavigationBar: BottomNavigationBar(
+        backgroundColor: Color(0xffFFFFFF),
+        selectedItemColor: Colors.black,
+        unselectedItemColor: Color(0xFF70757F),
+        currentIndex: _currentIndex,
+        onTap: (index) => setState(() => _currentIndex = index),
+        showUnselectedLabels: true,
+        type: BottomNavigationBarType.fixed,
+        items: const [
+          BottomNavigationBarItem(
+            icon: ImageIcon(AssetImage('assets/images/icons/House.png')),
+            label: 'Главная',
+          ),
+          BottomNavigationBarItem(
+            icon: ImageIcon(
+              AssetImage('assets/images/icons/MagnifyingGlass.png'),
+            ),
+            label: 'Каталог',
+          ),
+          BottomNavigationBarItem(
+            icon: ImageIcon(
+              AssetImage('assets/images/icons/ShoppingCartSimple.png'),
+            ),
+            label: 'Корзина',
+          ),
+          BottomNavigationBarItem(
+            icon: ImageIcon(AssetImage('assets/images/icons/User.png')),
+            label: 'Профиль',
+          ),
+        ],
+      ),
+    );
+  }
+}
--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -1,16 +1,6 @@
+import 'package:cosmetics_store/app/pages/main_screen.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MainApp());
-}
-
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(body: Center(child: Text('Hello World!'))),
-    );
-  }
+  runApp(MaterialApp(title: 'Cosmetics Shop', home: MainScreen()));
 }
